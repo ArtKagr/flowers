66901f85872fdfc13163c88486e2b911d8fcb8c7
kagr <artemiokrasz@yandex.ru>
2021-04-06 21:19:17 +0200

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -28,77 +28,83 @@
       </div>
     </section>
     <section class="page-video" />
-    <section class="page-subscription">
+    <section class="page-subscription" id="rate">
       <h2 class="page-subscription-title">Выберите свою подписку</h2>
       <div class="page-subscription-container">
-        <div class="page-subscription-container-card -first">
-          <div class="page-subscription-container-card-image">
-            <div class="page-subscription-container-card-image-sale">
-              <span class="page-subscription-container-card-image-sale-title">ВЫ ЭКОНОМИТЕ</span>
-              <span class="page-subscription-container-card-image-sale-cost">3 760 ₽</span>
-            </div>
-          </div>
-          <div class="page-subscription-container-card-description">
-            <span class="page-subscription-container-card-description-title">Standart</span>
-            <span class="page-subscription-container-card-description-subtitle">
-              Lorem ipsum ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </span>
-            <div class="page-subscription-container-card-description-container">
-              <span class="page-subscription-container-card-description-container-old_price">3 900 ₽</span>
-              <div class="page-subscription-container-card-description-container-new_price">
-                <span class="page-subscription-container-card-description-container-new_price-title">5 900 ₽</span>
-                <span class="page-subscription-container-card-description-container-new_price-subtitle">за 10 доставок в течение года</span>
+        <a href="#current_card" class="custom_link">
+          <div class="page-subscription-container-card -first">
+            <div class="page-subscription-container-card-image">
+              <div class="page-subscription-container-card-image-sale">
+                <span class="page-subscription-container-card-image-sale-title">ВЫ ЭКОНОМИТЕ</span>
+                <span class="page-subscription-container-card-image-sale-cost">3 760 ₽</span>
               </div>
             </div>
-            <b-button variant="outline-primary" class="page-subscription-container-card-description-button">В корзину</b-button>
-          </div>
-        </div>
-        <div class="page-subscription-container-card -second">
-          <div class="page-subscription-container-card-image">
-            <div class="page-subscription-container-card-image-sale">
-              <span class="page-subscription-container-card-image-sale-title">ВЫ ЭКОНОМИТЕ</span>
-              <span class="page-subscription-container-card-image-sale-cost">3 760 ₽</span>
-            </div>
-          </div>
-          <div class="page-subscription-container-card-description">
-            <span class="page-subscription-container-card-description-title">Standart</span>
-            <span class="page-subscription-container-card-description-subtitle">
+            <div class="page-subscription-container-card-description">
+              <span class="page-subscription-container-card-description-title">Standart</span>
+              <span class="page-subscription-container-card-description-subtitle">
               Lorem ipsum ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </span>
-            <div class="page-subscription-container-card-description-container">
-              <span class="page-subscription-container-card-description-container-old_price">3 900 ₽</span>
-              <div class="page-subscription-container-card-description-container-new_price">
-                <span class="page-subscription-container-card-description-container-new_price-title">5 900 ₽</span>
-                <span class="page-subscription-container-card-description-container-new_price-subtitle">за 10 доставок в течение года</span>
+              <div class="page-subscription-container-card-description-container">
+                <span class="page-subscription-container-card-description-container-old_price">3 900 ₽</span>
+                <div class="page-subscription-container-card-description-container-new_price">
+                  <span class="page-subscription-container-card-description-container-new_price-title">5 900 ₽</span>
+                  <span class="page-subscription-container-card-description-container-new_price-subtitle">за 10 доставок в течение года</span>
+                </div>
+              </div>
+              <b-button variant="outline-primary" class="page-subscription-container-card-description-button">В корзину</b-button>
+            </div>
+          </div>
+        </a>
+        <a href="#current_card" class="custom_link">
+          <div class="page-subscription-container-card -second">
+            <div class="page-subscription-container-card-image">
+              <div class="page-subscription-container-card-image-sale">
+                <span class="page-subscription-container-card-image-sale-title">ВЫ ЭКОНОМИТЕ</span>
+                <span class="page-subscription-container-card-image-sale-cost">3 760 ₽</span>
               </div>
             </div>
-            <b-button variant="outline-primary" class="page-subscription-container-card-description-button">В корзину</b-button>
-          </div>
-        </div>
-        <div class="page-subscription-container-card -third">
-          <div class="page-subscription-container-card-image">
-            <div class="page-subscription-container-card-image-sale">
-              <span class="page-subscription-container-card-image-sale-title">ВЫ ЭКОНОМИТЕ</span>
-              <span class="page-subscription-container-card-image-sale-cost">3 760 ₽</span>
-            </div>
-          </div>
-          <div class="page-subscription-container-card-description">
-            <span class="page-subscription-container-card-description-title">Standart</span>
-            <span class="page-subscription-container-card-description-subtitle">
+            <div class="page-subscription-container-card-description">
+              <span class="page-subscription-container-card-description-title">Standart</span>
+              <span class="page-subscription-container-card-description-subtitle">
               Lorem ipsum ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </span>
-            <div class="page-subscription-container-card-description-container">
-              <span class="page-subscription-container-card-description-container-old_price">3 900 ₽</span>
-              <div class="page-subscription-container-card-description-container-new_price">
-                <span class="page-subscription-container-card-description-container-new_price-title">5 900 ₽</span>
-                <span class="page-subscription-container-card-description-container-new_price-subtitle">за 10 доставок в течение года</span>
+              <div class="page-subscription-container-card-description-container">
+                <span class="page-subscription-container-card-description-container-old_price">3 900 ₽</span>
+                <div class="page-subscription-container-card-description-container-new_price">
+                  <span class="page-subscription-container-card-description-container-new_price-title">5 900 ₽</span>
+                  <span class="page-subscription-container-card-description-container-new_price-subtitle">за 10 доставок в течение года</span>
+                </div>
+              </div>
+              <b-button variant="outline-primary" class="page-subscription-container-card-description-button">В корзину</b-button>
+            </div>
+          </div>
+        </a>
+        <a href="#current_card" class="custom_link">
+          <div class="page-subscription-container-card -third">
+            <div class="page-subscription-container-card-image">
+              <div class="page-subscription-container-card-image-sale">
+                <span class="page-subscription-container-card-image-sale-title">ВЫ ЭКОНОМИТЕ</span>
+                <span class="page-subscription-container-card-image-sale-cost">3 760 ₽</span>
               </div>
             </div>
-            <b-button variant="outline-primary" class="page-subscription-container-card-description-button">В корзину</b-button>
+            <div class="page-subscription-container-card-description">
+              <span class="page-subscription-container-card-description-title">Standart</span>
+              <span class="page-subscription-container-card-description-subtitle">
+              Lorem ipsum ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </span>
+              <div class="page-subscription-container-card-description-container">
+                <span class="page-subscription-container-card-description-container-old_price">3 900 ₽</span>
+                <div class="page-subscription-container-card-description-container-new_price">
+                  <span class="page-subscription-container-card-description-container-new_price-title">5 900 ₽</span>
+                  <span class="page-subscription-container-card-description-container-new_price-subtitle">за 10 доставок в течение года</span>
+                </div>
+              </div>
+              <b-button variant="outline-primary" class="page-subscription-container-card-description-button">В корзину</b-button>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
-      <div class="page-subscription-current_card">
+      <div class="page-subscription-current_card" id="current_card">
         <div class="page-subscription-current_card-gallery">
           <div class="page-subscription-current_card-gallery-main_photo" />
           <div class="page-subscription-current_card-gallery-photos">
@@ -214,7 +220,7 @@
 
       </div>
     </section>
-    <section class="page-reviews">
+    <section class="page-reviews" id="reviews">
       <h2 class="page-reviews-title">Отзывы клиентов</h2>
       <div class="page-reviews-container">
         <div class="page-reviews-container-card -first">
@@ -246,7 +252,7 @@
         </div>
       </div>
     </section>
-    <section class="page-think">
+    <section class="page-think" id="think">
       <h2 class="page-think-title">Что нужно знать</h2>
       <div class="page-think-container">
         <div class="page-think-container-item">
@@ -326,7 +332,7 @@
         </div>
       </div>
     </section>
-    <section class="page-best_flowers">
+    <section class="page-best_flowers" id="best_flowers">
       <h2 class="page-best_flowers-title">Лучшие цветы, выбранные для вас</h2>
       <div class="page-best_flowers-container">
         <div class="page-best_flowers-container-main_image" />
